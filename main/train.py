09dea3538f32181060bb814f4a322dcc2f69bfde
jungg <jungkwen20@gmail.com>
2025-07-19 02:16:48 +0900

    Implements base study pipeline

--- main/train.py
+++ main/train.py
@@ -1,4 +1,5 @@
 import os
+import torch
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import SubprocVecEnv
 from stable_baselines3.common.callbacks import CheckpointCallback
@@ -15,11 +16,15 @@ if __name__ == '__main__':
     num_cpu = 4
     vec_env = SubprocVecEnv([make_env() for i in range(num_cpu)])
 
+    # GPU 사용 가능 여부 확인
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'Using device: {device}')
+
     # 체크포인트 콜백 설정: 10,000 스텝마다 모델 저장
     checkpoint_callback = CheckpointCallback(save_freq=10000, save_path=model_dir, name_prefix='ppo_qwop')
 
     # PPO 모델 정의
-    model = PPO('MlpPolicy', vec_env, verbose=1, tensorboard_log=log_dir)
+    model = PPO('MlpPolicy', vec_env, verbose=1, tensorboard_log=log_dir, device=device)
 
     # 모델 학습 시작
     model.learn(total_timesteps=1000000, callback=checkpoint_callback)
